e78e2bd71445549180531f55ac78f24b97b5e887
Bart <Bart@LenovoU410>
2014-04-01 13:43:01 +0200

    Start Quixo

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("lolololll");
+        System.out.println("Welkom bij Quixo!");
     }
 
 }
