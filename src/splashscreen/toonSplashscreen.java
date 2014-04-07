ba047a3a883159e749d02bc1dd64190e79f459fe
Bart <Bart@LenovoU410>
2014-04-08 00:20:19 +0200

    werkend maken splashscreen + toevoegen toonHoofdmenu.

--- src/splashscreen/toonSplashscreen.java
+++ src/splashscreen/toonSplashscreen.java
@@ -1,18 +1,38 @@
 package splashscreen;
 
-import hoofdmenu.Hoofdmenu;
+import hoofdmenu.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
 
 /**
  * Created by Bart on 7-4-2014.
  */
 public class toonSplashscreen {
 
-    private Hoofdmenu hoofdmenu;
+    private Timer timer;
+    private Splashscreen splashscreen;
 
     public toonSplashscreen(){
-        this.hoofdmenu = new Hoofdmenu();
+
+        splashscreen = new Splashscreen();
+
+        TimeListener tm = new TimeListener();
+        timer = new Timer(3000, tm);
+        timer.start();
+
     }
 
+    class TimeListener implements ActionListener{
 
+        @Override
+        public void actionPerformed(ActionEvent e) {
 
+            splashscreen.setVisible(false);
+            new toonHoofdmenu();
+            timer.stop();
+
+        }
+    }
 }
