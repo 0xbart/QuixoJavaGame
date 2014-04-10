cf3951578d3be0fd441c7feae1477ab8592871c5
Bart <Bart@LenovoU410>
2014-04-10 12:13:46 +0200

    File wijzigingen (naam etc.)

--- src/hoofdmenu/ToonHoofdmenu.java
+++ src/hoofdmenu/ToonHoofdmenu.java
@@ -1,7 +1,45 @@
 package hoofdmenu;
 
+import utils.Task;
+
+import javax.swing.*;
+
 /**
- * Created by Bart on 10-4-2014.
+ * Created by Bart on 9-4-2014.
  */
-public class ToonHoofdmenu {
+public class ToonHoofdmenu extends Task {
+
+    private JFrame spelFrame;
+    private boolean validate = false;
+
+    public ToonHoofdmenu(JFrame spelFrame, boolean validate){
+
+        this.spelFrame = spelFrame;
+        this.validate = validate;
+    }
+
+    public void run() {
+
+        boolean validate = validate();
+        if(validate == true)
+        {
+            execute();
+        }
+    }
+
+    private boolean validate() {
+
+        if(validate == true) {
+            return true;
+        } else {
+            return true;
+        }
+    }
+
+    private void execute() {
+
+        spelFrame.setContentPane(new Hoofdmenu(spelFrame));
+        spelFrame.validate();
+    }
+
 }
--- src/splashscreen/ToonSplashscreen.java
+++ src/splashscreen/ToonSplashscreen.java
@@ -1,7 +1,59 @@
 package splashscreen;
 
+import hoofdmenu.ToonHoofdmenu;
+import utils.*;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
- * Created by Bart on 10-4-2014.
+ * Created by Bart on 9-4-2014.
  */
-public class ToonSplashscreen {
+public class ToonSplashscreen extends Task {
+
+    private JFrame spelFrame;
+    private Timer timer;
+    private boolean validate = true;
+
+    public ToonSplashscreen(JFrame spelFrame){
+
+        this.spelFrame = spelFrame;
+    }
+
+    public void run() {
+
+        boolean validate = validate();
+
+        if(validate == true)
+        {
+            execute();
+        }
+    }
+
+    private boolean validate() {
+        return true;
+    }
+
+    private void execute() {
+
+        spelFrame.setContentPane(new Splashscreen(spelFrame));
+
+        TimeListener tm = new TimeListener();
+        timer = new Timer(300, tm);
+        timer.start();
+
+        validate = true;
+    }
+
+    class TimeListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            ToonHoofdmenu toonHoofdmenu = new ToonHoofdmenu(spelFrame, validate);
+            toonHoofdmenu.run();
+            timer.stop();
+        }
+    }
 }
