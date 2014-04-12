89b14d1c68d799247e814b4b79255f371ad24bb7
Bart <Bart@LenovoU410>
2014-04-12 13:14:01 +0200

    Toevoegen buttons profiel laden

--- src/hoofdmenu/laadprofiel/LaadProfiel.java
+++ src/hoofdmenu/laadprofiel/LaadProfiel.java
@@ -1,5 +1,7 @@
 package hoofdmenu.laadprofiel;
 
+import hoofdmenu.ToonHoofdmenu;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -11,6 +13,7 @@ import java.awt.event.ActionListener;
 public class LaadProfiel extends JPanel implements ActionListener {
 
     private JFrame spelFrame;
+    private JButton cancel, ok;
     private JLabel background;
 
     public LaadProfiel(JFrame spelFrame) {
@@ -18,6 +21,7 @@ public class LaadProfiel extends JPanel implements ActionListener {
         this.spelFrame = spelFrame;
 
         maakAchtergrond();
+        maakButtons();
 
         setLayout(new BorderLayout());
         add(background);
@@ -27,8 +31,32 @@ public class LaadProfiel extends JPanel implements ActionListener {
         background = new JLabel(new ImageIcon("src/resources/achtergrond/profiel_bg.png"));
     }
 
+    private void maakButtons() {
+
+        cancel = new JButton(new ImageIcon("src/resources/buttons/cancel.png"));
+        cancel.setRolloverIcon(new ImageIcon("src/resources/buttons/cancel_h.png"));
+        cancel.setBorder(null);
+        cancel.setBounds(100, 415, 150, 51);
+        cancel.addActionListener(this);
+
+        ok = new JButton(new ImageIcon("src/resources/buttons/ok.png"));
+        ok.setRolloverIcon(new ImageIcon("src/resources/buttons/ok_h.png"));
+        ok.setBorder(null);
+        ok.setBounds(300, 415, 150, 51);
+        ok.addActionListener(this);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if(e.getSource() == cancel){
+
+            ToonHoofdmenu toonHoofdmenu = new ToonHoofdmenu(spelFrame, true);
+            toonHoofdmenu.run();
+
+        } else if(e.getSource() == ok){
+
+            System.out.println("OK is geklikt.");
+        }
     }
 }
