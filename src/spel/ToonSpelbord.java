dba3f018485955fdd0b74f7dd32d429488727ec3
Axel Boesenach <axelboesenach@outlook.com>
2014-04-15 17:38:56 +0200

    Klein begin gemaakt aan spelbord

--- src/spel/ToonSpelbord.java
+++ src/spel/ToonSpelbord.java
@@ -3,6 +3,7 @@ package spel;
 import utils.Task;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 /**
  * Created by Bart on 15-4-2014.
@@ -37,4 +38,24 @@ public class ToonSpelbord extends Task {
         spelFrame.validate();
     }
 
+    //Axel Boesenach (nog lang niet af)
+
+    private String spelerRondje, spelerKruisje;
+    private int[] spelBord = {
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+    };
+
+    ArrayList<SpelBord> Bord  = new ArrayList<SpelBord>();
+
+    public int[] getSpelBord() {
+        return spelBord;
+    }
+
+    public void setSpelBord(int[] spelBord) {
+        this.spelBord = spelBord;
+    }
 }
