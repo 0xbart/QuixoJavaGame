204a81c16392191cc5f657d85cf5197759cc2c6c
Bart <Bart@LenovoU410>
2014-04-15 15:57:12 +0200

    achtergrond spelveld + toepassingen constructor

--- src/spel/SpelBord.java
+++ src/spel/SpelBord.java
@@ -8,9 +8,16 @@ import javax.swing.*;
 public class SpelBord extends JPanel {
 
     private JFrame spelFrame;
+    private JLabel background;
 
     public SpelBord(JFrame spelFrame) {
 
         this.spelFrame = spelFrame;
+
+        maakAchtergrond();
+    }
+
+    private void maakAchtergrond(){
+        background = new JLabel(new ImageIcon("src/resources/achtergrond/spelveld_bg.png"));
     }
 }
