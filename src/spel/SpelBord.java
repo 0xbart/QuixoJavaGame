ea695204178bcc6051e0166d83071878c885d959
Axel Boesenach <axelboesenach@outlook.com>
2014-04-17 14:48:59 +0200

    Onder maakbuttons de JToggleButton gebruikt ipv JButton. JToggleButton heeft ingebouwde listeners en is veelgebruikt in games.

--- src/spel/SpelBord.java
+++ src/spel/SpelBord.java
@@ -15,9 +15,11 @@ public class SpelBord extends JPanel implements MouseListener {
 
     private JFrame spelFrame;
     private JLabel background, help, menu;
-    private JLabel[] blokken = new JLabel[25];
+    private JToggleButton[] blokken = new JToggleButton[25];
 
-    public SpelBord(JFrame spelFrame) {
+
+
+    public SpelBord (JFrame spelFrame ) {
 
         this.spelFrame = spelFrame;
 
@@ -47,26 +49,32 @@ public class SpelBord extends JPanel implements MouseListener {
     }
 
     private void maakMenu() {
-       menu = new JLabel(new ImageIcon("src/resources/buttons/menu.png"));
-       menu.setBounds(15, 468, 121, 48);
-       menu.setBorder(null);
-       menu.addMouseListener(this);
+        menu = new JLabel(new ImageIcon("src/resources/buttons/menu.png"));
+        menu.setBounds(15, 468, 121, 48);
+        menu.setBorder(null);
+        menu.addMouseListener(this);
 
     }
 
+    //Voorzien van JToggleButton zodat er geen aparte listeners gebruikt hoeven te worden.
     private void maakButtons() {
 
         int c = 0;
 
         for(int i = 0; i < 5; i++){
             for(int j = 0; j < 5; j++){
-                blokken[c] = new JLabel(new ImageIcon("src/resources/spel/1.png"));
+                blokken[c] = new JToggleButton(new ImageIcon("src/resources/spel/1.png"));
+                blokken[c].setRolloverIcon(new ImageIcon("src/resources/spel/5.png"));
+                blokken[c].setPressedIcon(new ImageIcon("src/resources/spel/3.png"));
+                blokken[c].setDisabledIcon(new ImageIcon("src/resources/spel/4.png"));
                 blokken[c].setBounds(130 + (j*57), 148 + (i*57), 57, 57);
                 blokken[c].setBorder(null);
                 c++;
+
             }
         }
     }
+
 
     private void toevoegenButtons() {
 
@@ -74,6 +82,8 @@ public class SpelBord extends JPanel implements MouseListener {
             add(blokken[i]);
         }
     }
+
+
 
 
     @Override
@@ -94,17 +104,16 @@ public class SpelBord extends JPanel implements MouseListener {
     @Override
     public void mousePressed(MouseEvent e) {
 
+
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
 
-
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-
 
     }
 
